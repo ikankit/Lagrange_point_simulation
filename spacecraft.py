b6c51b780cd8d9c75956c45c703021177af2e5d6
Sucheta <skylar.sucheta0517@gmail.com>
2026-08-07 15:52:11 +0530

    Refactor spacecraft.py by removing unused imports

--- spacecraft.py
+++ spacecraft.py
@@ -3,9 +3,6 @@ import numpy as np
 from Initial_conditions import craft_m
 
 from body import Body
-from sun_and_earth import create_bodies
-
-sun,earth=create_bodies()
 
 def create_spacecraft(position,velocity):
     
