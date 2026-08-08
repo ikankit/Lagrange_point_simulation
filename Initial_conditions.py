c3d97f468c3815fd14a381ab384488ce0354108c
Ankit <ankitberanarayana123@gmail.com>
2026-08-08 23:49:42 +0530

    Update Initial_conditions.py
    now mass insertion is available

--- Initial_conditions.py
+++ Initial_conditions.py
@@ -3,10 +3,15 @@ from math import sqrt
 # Physics Constants & Center of Mass 
 G = 1  #Gravitational coonstant
 R = 14  #Distance b/w earth and sun
+m_of_object1=0.97
 
-mu = 0.012  # Mass ratio
+m_of_object2=0.03
+
+mu =(m_of_object2)/(m_of_object1+m_of_object2) # Mass ratio
+
 m_earth = mu #Earth mass
 m_sun = 1.0 - mu #Sun mass
+
 omega = sqrt(G * 1.0 / R**3) #Angular velocity
 
 nudge = 0.15  #Small Displacement
@@ -14,4 +19,4 @@ craft_m = 0.000001  #Mass of spacecraft
 
 dt = 0.1    #No.of timesteps
 
-steps= 35000 #Total steps
+steps= 20000 #Total steps
